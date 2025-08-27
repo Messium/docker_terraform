939b97afdb25f9db584ad7e9741bb19b8af1f846
Messium <danielholstrom@gmail.com>
2025-08-27 14:47:34 +0200

    better inventory

--- ansible_apply.sh
+++ ansible_apply.sh
@@ -1,5 +1,6 @@
 export ANSIBLE_HOST_KEY_CHECKING=False
 # https://stackoverflow.com/questions/42462435/ansible-provisioning-error-using-a-ssh-password-instead-of-a-key-is-not-possibl
 # https://www.geeksforgeeks.org/devops/disabling-host-key-checking-in-ansible/
-ansible-playbook -i inventory playbook.yaml
+ansible-playbook generate_inventory.yml 
+ansible-playbook -i inventory.yaml playbook.yaml
 ./connect.sh
